d1c9a8354fd6fec16c39bcac1383cdee3a3b25c6
Jack Maloney <jmaloney@axonify.com>
2021-12-09 21:48:08 -0500

    wip

--- 2021/9/main.py
+++ 2021/9/main.py
@@ -1,13 +1,19 @@
 if __name__ == '__main__':
-    with open('test_input') as f:
-        height_map = [l.strip() for l in f]
+    with open('input') as f:
+        height_map = ['9' + l.strip() + '9' for l in f]
 
-        for x in height_map:
-            for i in range(len(x)):
-                w = x[max(0, i-1):min(len(x), i+2)]
+        # wrap in 9s
+        height_map.insert(0, '9' * len(height_map[0]))
+        height_map.append('9' * len(height_map[0]))
 
-                if i == 0:
+        t = 0
+        for y in range(1, len(height_map)-1):
+            line = height_map[y]
+            for i in range(1, len(line)-1):
+                w_y = [h[i] for h in height_map[y-1:y+2]]
+                w_x = line[i-1:i+2]
+                if w_x[0] > w_x[1] < w_x[2] and w_y[0] > w_y[1] < w_y[2]:
+                    print((w_x[1], (i, y)))
+                    t += int(w_x[1]) + 1
 
-                print(w)
-
-        print(height_map)
+        print(t)
--- 2021/9/main_2.py
+++ 2021/9/main_2.py
@@ -1,86 +1,44 @@
-def update_segment_dict(i, used_segs, segment_dict):
-    used_segs = set(used_segs)
-    i = set(i)
-    for s in set('abcdefg') - i:
-        segment_dict[s] = segment_dict[s] - used_segs
-    for s in i:
-        segment_dict[s] = segment_dict[s].intersection(used_segs)
+def get_adjacent_points(x, y, height_map):
+    w_y = [(height_map[i][x], x, i) for i in range(y-1, y+2)]
+    w_x = [(height_map[y][i], i, y) for i in range(x-1, x+2)]
+    w_y.pop(1)
+    w_x.pop(1)
+    return w_x, w_y
 
-    for key in segment_dict:
-        if len(segment_dict[key]) == 1:
-            for other_key in segment_dict:
-                if key != other_key:
-                    segment_dict[other_key] = segment_dict[other_key] - set(segment_dict[key])
+
+def search_basin(x, y, height_map):
+    p = height_map[y][x]
+    if p == 9:
+        return 0
+    else:
+        t = 0
+        adj_x, adj_y = get_adjacent_points(x, y, height_map)
+        for adj in adj_x:
+            t += search_basin(adj[1], adj[2], height_map)
+        for adj in adj_y:
+            t += search_basin(adj[1], adj[2], height_map)
+        return t
 
 
 if __name__ == '__main__':
-    total = 0
-    with open('input') as f:
+    with open('test_input') as f:
+        height_map = ['9' + l.strip() + '9' for l in f]
 
-        values = {
-            'abcefg': 0,
-            'cf': 1,
-            'acdeg': 2,
-            'acdfg': 3,
-            'bcdf': 4,
-            'abdfg': 5,
-            'abdefg': 6,
-            'acf': 7,
-            'abcdefg': 8,
-            'abcdfg': 9,
-        }
-        for line in f:
-            s = line.strip().split(' | ')
-            signal_pattern = s[0].split(' ')
-            output = s[1].split(' ')
+        # wrap in 9s
+        height_map.insert(0, '9' * len(height_map[0]))
+        height_map.append('9' * len(height_map[0]))
 
-            # variables named after segment count
-            two = list(map(set, filter(lambda p: len(p) == 2, signal_pattern)))[0]
-            three = list(map(set, filter(lambda p: len(p) == 3, signal_pattern)))[0]
-            four = list(map(set, filter(lambda p: len(p) == 4, signal_pattern)))[0]
-            fives = list(map(set, filter(lambda p: len(p) == 5, signal_pattern)))
-            sixes = list(map(set, filter(lambda p: len(p) == 6, signal_pattern)))
-            seven = list(map(set, filter(lambda p: len(p) == 7, signal_pattern)))[0]
+        t = 0
+        low_points = []
+        for y in range(1, len(height_map)-1):
+            line = height_map[y]
+            for x in range(1, len(line)-1):
+                w_x, w_y = get_adjacent_points(x, y, height_map)
+                if w_x[0][0] > line[x] < w_x[1][0] and w_y[0][0] > line[x] < w_y[1][0]:
+                    t += int(line[x]) + 1
+                    low_points.append((line[x], x, y))
 
-            segment_dict = dict()
-            segment_dict['a'] = three - two
-            segment_dict['b'] = four - two
-            segment_dict['c'] = two
-            segment_dict['f'] = two
-            segment_dict['d'] = four - two
+        print(t)
+        print(low_points)
 
-            for s in sixes:
-                if len(s.difference(four.union(segment_dict['a']))) == 1:
-                    segment_dict['g'] = s.difference(four.union(segment_dict['a']))
-                    segment_dict['e'] = seven - s
-                    break
-
-            for s in sixes:
-                if s.union(two) == s and segment_dict['e'].union(s) == s:
-                    segment_dict['d'] = seven - s
-
-            segment_dict['b'] = segment_dict['b'] - segment_dict['d']
-
-            for s in sixes:
-                if s.union(two) != s:
-                    segment_dict['c'] = two - s
-                    segment_dict['f'] = two - segment_dict['c']
-
-            for key in segment_dict:
-                segment_dict[key] = segment_dict[key].pop()
-
-            flipped = dict()
-            for key in segment_dict:
-                flipped[segment_dict[key]] = key
-
-            t = 0
-            d = 1
-            for o in reversed(output):
-                o_v = ''.join(sorted([flipped[c] for c in o]))
-                t += values[o_v] * d
-                d *= 10
-            print(t)
-            total += t
-
-        print(total)
-
+        print(search_basin(low_points[0][1], low_points[0][2], height_map))
