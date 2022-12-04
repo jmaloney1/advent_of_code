de92414cbca8ed13b2a1ab7ac4ec7c9ffd4611fb
Jack Maloney <jackmaloney1996@gmail.com>
2022-12-04 09:20:38 -0500

    day 4

---
+++ 2022/4/main_2.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    with open('input') as f:
+        fully_containing = 0
+        for line in f:
+            section_1, section_2 = line.strip().split(',')[:2]
+            section_1 = tuple(map(int, section_1.split('-')))
+            section_2 = tuple(map(int, section_2.split('-')))
+            print(section_1)
+            print(section_2)
+
+            if section_1[0] <= section_2[1] and section_1[1] >= section_2[0]:
+                fully_containing += 1
+            elif section_1[1] <= section_2[0] and section_1[0] >= section_2[1]:
+                fully_containing += 1
+
+    print(fully_containing)
